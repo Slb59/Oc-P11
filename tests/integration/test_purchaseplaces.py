efc925db73c8c0ce634523b0318b71df47595a63
Slb59 <sylvie.bricout2@laposte.net>
2023-08-04 16:54:10 +0200

    add tests purchaseplaces errors

--- tests/integration/test_purchaseplaces.py
+++ tests/integration/test_purchaseplaces.py
@@ -1,4 +1,6 @@
 import gudlft.server as server
+
+from http import HTTPStatus
 from gudlft.server import app
 from gudlft.db import DataLoader
 
@@ -31,7 +33,7 @@ class TestPurchasePlaces:
             }
         )
         # print(server.data)
-        assert result.status_code == 200
+        assert result.status_code == HTTPStatus.OK
         assert server.data.competitions[0].number_of_places == \
             competition_places_before - places_booked
         assert server.data.clubs[0].points == \
@@ -39,4 +41,47 @@ class TestPurchasePlaces:
         assert server.data.clubs[0].points >= 0
 
     def test_not_enough_club_points(self):
-        ...
+        club_points_before = server.data.clubs[0].points
+        competition_places_before =\
+            server.data.competitions[0].number_of_places
+        places_booked = club_points_before + 1
+        result = self.client.post(
+            "/purchasePlaces",
+            data={
+                "places": places_booked,
+                "club": server.data.clubs[0].name,
+                "competition": server.data.competitions[0].name
+            }
+        )
+        assert result.status_code == HTTPStatus.OK
+        assert server.data.competitions[0].number_of_places == \
+            competition_places_before
+        assert server.data.clubs[0].points == \
+            club_points_before
+        data = result.data.decode()
+        assert "<li>Your club have not enough points</li>"\
+            in data
+
+    def test_competition_places_not_available(self):
+        server.data.clubs[0].points = 100
+        club_points_before = server.data.clubs[0].points
+        competition_places_before =\
+            server.data.competitions[0].number_of_places
+        places_booked = competition_places_before + 1
+        result = self.client.post(
+            "/purchasePlaces",
+            data={
+                "places": places_booked,
+                "club": server.data.clubs[0].name,
+                "competition": server.data.competitions[0].name
+            }
+        )
+        assert result.status_code == HTTPStatus.OK
+        assert server.data.competitions[0].number_of_places == \
+            competition_places_before
+        assert server.data.clubs[0].points == \
+            club_points_before
+        data = result.data.decode()
+        print(data)
+        assert "<li>The competition have not enough places available</li>"\
+            in data
