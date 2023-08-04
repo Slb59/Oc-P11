13c72102a37b3911263f2bf92b69438e8f54e34a
Slb59 <sylvie.bricout2@laposte.net>
2023-08-04 20:36:22 +0200

    add functionnal test story2

--- tests/fonctionnal_edge/test_booking_edge.py
+++ tests/fonctionnal_edge/test_booking_edge.py
@@ -2,32 +2,58 @@ import time
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.edge.service import Service
+from selenium.webdriver.common.by import By
+
+import gudlft.server as server
+from gudlft.db import DataLoader
 
 
 class TestStories:
 
     def setup_method(self):
-        # self.driver = webdriver.Edge("tests/fonctionnal_edge/msedgedriver.exe")
+
+        # load test data
+        # server.data = DataLoader(
+        #     club_file='test2_clubs.json',
+        #     competition_file='test2_competitions.json'
+        # )
+
+        #start webdriver
         options = Options()
-        # options.binary_location = r"tests/fonctionnal_edge/msedgedriver.exe"
-        # options = webdriver.EdgeOptions()
         options.use_chromium = True
-        # options.add_argument('--allow-running-insecure-content')
-        # options.add_argument("--ignore-certificate-errors")
-        service = Service(executable_path="tests/fonctionnal_edge/msedgedriver.exe")
-        self.driver = webdriver.Chrome(
-            # service=service,
+
+        service = Service(
+            executable_path="tests/fonctionnal_edge/msedgedriver.exe"
+            )
+        self.driver = webdriver.Edge(
+            service=service,
             options=options,
-            executable_path=r"tests/fonctionnal_edge/msedgedriver.exe"
-            # service=webdriver.edge.service.Service(executable_path="tests/fonctionnal_edge/msedgedriver.exe")
             )
         self.driver.get("http://127.0.0.1:5000/")
+        self.driver.minimize_window()
         self.driver.maximize_window()
-        time.sleep(20)
+        time.sleep(5)
 
     def teardown_method(self):
         self.driver.quit()
 
-    def test_story1_login_scenario1():
+    def test_story1_login_scenario1(self):
+        field_email = self.driver.find_element(By.ID, "email_field")
+        field_email.send_keys("john@simplylift.co")
+        time.sleep(2)
+        field_email.submit()
+        time.sleep(2)
+        assert "Welcome, john@simplylift.co" in self.driver.page_source
+
+    def test_story2_login_scanario2(self):
+        field_email = self.driver.find_element(By.ID, "email_field")
+        field_email.send_keys("unlonown@example.com")
+        time.sleep(2)
+        field_email.submit()
+        time.sleep(2)
+        assert "Cette adresse email n'est pas reconnue"\
+            in self.driver.page_source
+        
+    def test_story3_logout(self):
         ...
-    
+
